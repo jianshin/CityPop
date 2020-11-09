b92b77731d4eeb25bff3f0cecf650dcc905f2949
Jian Shin <jiaanshin@gmail.com>
2020-11-09 12:44:23 +0100

    added buttons to first screen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,30 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 
+
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>CityPop</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.js" />
+      <Button onPress={() => {searchByCity();}} title="SEARCH BY CITY"/>
+      <Button onPress={() => {searchByCountry();}} title="SEARCH BY COUNTRY"/>
     </View>
+  );
+}
+
+const searchByCity = () => {
+  return(
+    "city"
+  );
+}
+
+const searchByCountry = () => {
+  return(
+    "country"
   );
 }
 
